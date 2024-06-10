3e86cf5777567d9dd529af42b40ad14ce6cd6e2a
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-06-10 13:13:43 -0300

    Updates

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ window.addEventListener('load', ()=> {
             window.location. href = 'https://api.whatsapp.com/send?phone=5521975766577&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20da%20funer%C3%A1ria%20Santa%20Casa'; 
         },1000)
     }
+
+    const element = document.getElementById('phoneNumber');
+    const maskOptions = {
+    mask: '(00) 00000-0000'
+    };
+    const mask = IMask(element, maskOptions);
 })
 
 
@@ -31,9 +37,17 @@ document.getElementById("btnModalWhatsapp").addEventListener('click', ()=> {
           });
         return false
     }
+    const responseEmail = fetch('programas/enviaEmail.php', {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        method: 'POST',
+        body: JSON.stringify({
+            "nome"      : document.getElementById('recipient-name').value,
+            "telefone"  : encodeURI(document.getElementById('phoneNumber').value)
+        })
+    })
 
-    window.location.href = window.location.href + 'obrigado';
-
-
+    window.location.href = 'https://santacasacentral.com.br/obrigado';
     
 })
